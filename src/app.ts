b7c633c001b5addc31046465ba712d3c13de61b7
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-07-19 18:58:28 -0300

    added supertest

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from "express"
 import cors from "cors"
-import dotenv from "dotenv"
 import "express-async-errors"
 
 import routes from "./routes/index.js"
 import errorHandler from "./middlewares/errorHandler.js"
-
-dotenv.config()
 
 const app = express()
 
@@ -15,7 +12,4 @@ app.use(cors())
 app.use(routes)
 app.use(errorHandler)
 
-const port = +process.env.PORT || 4000
-app.listen(port, () => {
-  console.log("Server is running")
-})
+export default app
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv"
+
+import app from "./app.js"
+
+dotenv.config()
+
+const port = +process.env.PORT || 4000
+app.listen(port, () => {
+  console.log("Server is running")
+})
